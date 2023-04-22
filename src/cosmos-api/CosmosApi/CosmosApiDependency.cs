f4ece4d8c0de859542380d589c3cbf5766c7eb41
pmosk <pm@ggroupp.ru>
2023-04-22 22:05:45 +0400

    Improve Dependency Extensions

--- src/cosmos-api/CosmosApi/CosmosApiDependency.cs
+++ src/cosmos-api/CosmosApi/CosmosApiDependency.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net.Http;
+using Microsoft.Extensions.Configuration;
 using PrimeFuncPack;
 
 namespace GGroupp.Infra.Bot.Builder;
@@ -11,7 +12,7 @@ public static class CosmosApiDependency
     public static Dependency<ICosmosApiProvider> UseCosmosApi(
         this Dependency<HttpMessageHandler, CosmosApiOption> dependency)
     {
-        _ = dependency ?? throw new ArgumentNullException(nameof(dependency));
+        ArgumentNullException.ThrowIfNull(dependency);
 
         return dependency.Fold<ICosmosApiProvider>(CosmosApiProvider.Create);
     }
@@ -19,9 +20,28 @@ public static class CosmosApiDependency
     public static Dependency<ICosmosApiProvider> UseCosmosApi(
         this Dependency<HttpMessageHandler> dependency, Func<IServiceProvider, CosmosApiOption> optionResolver)
     {
-        _ = dependency ?? throw new ArgumentNullException(nameof(dependency));
-        _ = optionResolver ?? throw new ArgumentNullException(nameof(optionResolver));
+        ArgumentNullException.ThrowIfNull(dependency);
+        ArgumentNullException.ThrowIfNull(optionResolver);
 
         return dependency.With(optionResolver).Fold<ICosmosApiProvider>(CosmosApiProvider.Create);
     }
+
+    public static Dependency<ICosmosApiProvider> UseCosmosApi(
+        this Dependency<HttpMessageHandler> dependency, string sectionName = "CosmosDb")
+    {
+        ArgumentNullException.ThrowIfNull(dependency);
+
+        return dependency.With(ResolveOption).Fold<ICosmosApiProvider>(CosmosApiProvider.Create);
+
+        CosmosApiOption ResolveOption(IServiceProvider serviceProvider)
+            =>
+            serviceProvider.GetServiceOrThrow<IConfiguration>().GetRequiredSection(sectionName ?? string.Empty).ReadCosmosApiOption();
+    }
+
+    private static CosmosApiOption ReadCosmosApiOption(this IConfigurationSection section)
+        =>
+        new(
+            baseAddress: new(section["BaseAddressUrl"] ?? string.Empty),
+            masterKey: section["MasterKey"] ?? string.Empty,
+            databaseId: section["DatabaseId"] ?? string.Empty);
 }
